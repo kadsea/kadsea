5c949d5358e6e11aea92c4f323f99c338e6b7a22
unknown <kadseaorg@outlook.com>
2024-04-10 01:35:52 +0800

    fix:main

--- params/config.go
+++ params/config.go
@@ -42,7 +42,7 @@ var CheckpointOracles = map[common.Hash]*CheckpointOracleConfig{}
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:             big.NewInt(879),
+		ChainID:             big.NewInt(878),
 		HomesteadBlock:      big.NewInt(0),
 		DAOForkBlock:        nil,
 		DAOForkSupport:      true,
